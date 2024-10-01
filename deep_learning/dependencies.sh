a57bfff2d5512bfe270e615d0936a7296b078415
ChantaClown <elpablo863@gmail.com>
2024-10-01 18:57:24 +0200

    DL lab 3

--- deep_learning/dependencies.sh
+++ deep_learning/dependencies.sh
@@ -3,16 +3,17 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install python3 if not installed
-sudo apt install -y python3 python3-venv python3-pip
+sudo apt install -y python3.10 python3.10-venv python3-pip
 
 # Generating a venv
-python3 -m venv Deep-Learning
+python3.10 -m venv Deep-Learning
 
 # Activating the virtual environment
 source Deep-Learning/bin/activate
 
 # Dependencies
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install keras tensorflow
 
 echo "Environment setup is complete."
+
